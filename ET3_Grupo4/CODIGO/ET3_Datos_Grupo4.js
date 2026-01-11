0deb5a4ceb72792f1f725ce7c960176a4a5245a1
FGPrado23 <fgprado23@esei.uvigo.es>
2026-01-11 13:36:27 +0100

    Update ET3_Datos_Grupo4.js

--- ET3_Grupo4/CODIGO/ET3_Datos_Grupo4.js
+++ ET3_Grupo4/CODIGO/ET3_Datos_Grupo4.js
@@ -1,6 +1,6 @@
 const def_datos_Grupo4 = Array(
-    Array('ET3', 'Isaías Gil Rodríguez', 'DNI', '41'),
-    Array('ET3', 'Paloma González Herrera', 'DNI', '39'),
-    Array('ET3', 'Xián González Pavón', 'DNI', '40'),
+    Array('ET3', 'Isaías Gil Rodríguez', '35674059R', '41'),
+    Array('ET3', 'Paloma González Herrera', '44088087P', '54'),
+    Array('ET3', 'Xián González Pavón', '54231669S', '40'),
     Array('ET3', 'Fernando García Prado', '35487019C', '37')
 );
